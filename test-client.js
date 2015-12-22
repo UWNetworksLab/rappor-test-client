d19fd300868244d5d509bb4e5a066a93e18e6b5f
Lally Singh <yell@lal.ly>
2015-12-22 11:04:13 -0500

    Now uploads.

--- test-client.js
+++ test-client.js
@@ -1,15 +1,57 @@
 var process = require("process");
 var freedom = require("freedom-for-node");
 var randgen = require('randgen');
+var request = require('request');
 var argv = require('yargs')
-    .usage('Usage: $0 [-m mean] [-v variance] [-n num_samples]')
+    .usage('Usage: $0 [-m mean] [-v variance] [-n num_samples] [-s] [-u host] [-p path] [-V variable]')
     .default('mean', 9)
     .default('variance', 4)
     .default('samples', 10)
+    .default('submit', false)
+    .default('host', 'https://www.uproxy.org')
+    .default('path', 'submit-rappor-stats')
+    .default('variable', 'test-v1')
     .alias('m', 'mean')
     .alias('v', 'variance')
     .alias('n', 'samples')
+    .alias('h', 'host')
+    .alias('p', 'path')
+    .alias('V', 'variable')
+    .alias('s', 'submit')
     .argv;
+
+function post_values(val) {
+    var path = argv.host + "/" + argv.path;
+    console.log("Posting " + Object.keys(val).length + " items to " + path);
+    function post_value_n(idx) {
+        var params = {};
+        params[argv.variable] = val[Object.keys(val)[idx]];
+        var param_str = JSON.stringify(params);
+        var options = {
+            method: 'post',
+            body: params,
+            json: true,
+            url: path
+        }
+        console.log("Posting " + JSON.stringify(params));
+        request(options, function (err, res, body) {
+            if (err) {
+                inspect(err, 'error posting json')
+                return
+            }
+            var headers = res.headers
+            var statusCode = res.statusCode
+            if (statusCode != 200) {
+                console.log("Failed: " + JSON.stringify(res));
+            } else if ((1+idx) < Object.keys(val).length) {
+                post_value_n(idx + 1);
+            } else {
+                process.exit(0);
+            }
+        })
+    }
+    post_value_n(0);
+}
 
 freedom.freedom("node_modules/freedomjs-anonymized-metrics/anonmetrics.json", {}).then(
     function (rappor_proto) {
@@ -42,8 +84,14 @@ freedom.freedom("node_modules/freedomjs-anonymized-metrics/anonmetrics.json", {}
             metric.retrieve().then(
                 function(val) {
                     console.log("Samples: " + samples);
-                    console.log(val);
-                    process.exit(0);
+                    for (var k in val) {
+                        console.log("  " + val[k]);
+                    }
+                    if (argv.submit) {
+                        post_values(val);
+                    } else {
+                        process.exit(0);
+                    }
                 });
         });
     });
